c7980fd3616b7b1d206a5c9545ded6f76b318559
t3rr0r <mail@t3rr0r.com>
2014-03-19 18:04:43 -0400

    typo

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var winston = require('winston');
 var Mail = require('winston-mail').Mail;
 
 module.exports = function(options) {
-  var logger = new(winston.Logger)({
-    transports: [
-      new(winston.transports.Console)({
-        level: options.level,
-        colorize: 'true'
-      })
-    ]
-  });
+    var logger = new(winston.Logger)({
+	transports: [
+	    new(winston.transports.Console)({
+		level: options.level,
+		colorize: 'true'
+	    })
+	]
+    });
 
-  if (options.file) {
-    logger.add(winston.transports.File, options.file);
-  }
+    if (options.file) {
+	logger.add(winston.transports.File, options.file);
+    }
 
-  if (options.mail) {
-    logger.add(Mail, options.logging.mail);
-  }
+    if (options.mail) {
+	logger.add(Mail, options.mail);
+    }
 
-  return logger;
+    return logger;
 
 };
